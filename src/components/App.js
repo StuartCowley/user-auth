5aba98799c09021a40db22dc37837ae04be894a8
StuartCowley <voidstalker@live.co.uk>
2022-07-19 12:52:44 +0100

    Add navigation and routing to app and update home component

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,29 @@
-import React, { useState } from 'react';
-import Home from './Home';
-import '../styles/app.css';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route, NavLink } from "react-router-dom";
+import Home from "./Home";
+import LoginForm from "./LoginForm";
+import "../styles/app.css";
+import SignupForm from "./SignupForm";
 
 const App = () => {
   const [user, setUser] = useState({});
 
   return (
     <div className="app">
-      <Home user={user} setUser={setUser} />
+      <Router>
+        <nav className="app__navbar">
+          <NavLink className="app__nav-item" to="/">Home</NavLink>
+          <NavLink className="app__nav-item" to="signup">Signup</NavLink>
+          <NavLink className="app__nav-item" to="login">Login</NavLink>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home user={user} />} />
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/signup" element={<SignupForm setUser={setUser} />} />
+        </Routes>
+      </Router>
     </div>
+
   );
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import SignupForm from './SignupForm';
+import React from "react";
+import { Link } from "react-router-dom";
 
 import "../styles/home.css"
 
-const Home = ({ user, setUser }) => {
+const Home = ({ user }) => {
   return (
     <div className="home">
       {user.firstName ? (
-        <div>Welcome to you, {user.firstName}</div>
-      ) : <SignupForm setUser={setUser} />}
+        <h2>Welcome to you, <span className="home__username">{user.firstName}</span></h2>
+      ) : (
+        <h2>
+          You are not logged in, <Link to="login">log in</Link> or <Link to="signup">signup</Link>
+        </h2>
+      )}
     </div>
   )
 }
