c47daee5f7e767ae80e3827aee34533e8164076c
StuartCowley <voidstalker@live.co.uk>
2022-07-20 13:38:02 +0100

    update tests and snapshots for login and signup components

--- src/__tests__/LoginForm.test.js
+++ src/__tests__/LoginForm.test.js
@@ -4,108 +4,153 @@ import renderer from "react-test-renderer";
 import LoginForm from '../components/LoginForm';
 
 describe("Login form component", () => {
-  const validProps = {
-    allUsers: [],
-    setCurrentUser: jest.fn()
-  };
+  describe("User is logged in", () => {
+    const validProps = {
+      allUsers: [
+        {
+          firstName: "testFirst",
+          lastName: "testSecond",
+          email: "test@email.com",
+          password: "asdfgh"
+        }
+      ],
+      setCurrentUser: jest.fn(),
+      currentUser: {
+        firstName: "testFirst",
+        lastName: "testSecond",
+        email: "test@email.com",
+        password: "asdfgh"
+      }
+    };
 
-  it("matches snapshot", () => {
-    const generated = renderer
-      .create(
+    it("renders already logged in message", () => {
+      render(
         <Router>
           <LoginForm
             allUsers={validProps.allUsers}
             setCurrentUser={validProps.setCurrentUser}
+            currentUser={validProps.currentUser}
           />
         </Router>
-      ).toJSON();
+      );
 
-    expect(generated).toMatchSnapshot();
+      const title = screen.getByText("You are already logged in, testFirst!");
+
+      expect(title).toBeInTheDocument;
+    })
   })
 
-  it("renders title", () => {
-    render(
-      <Router>
-        <LoginForm
-          allUsers={validProps.allUsers}
-          setCurrentUser={validProps.setCurrentUser}
-        />
-      </Router>
-    );
-    const title = screen.getByText(/Login here:/);
+  describe("User is not logged in", () => {
+    const validProps = {
+      allUsers: [],
+      setCurrentUser: jest.fn(),
+      currentUser: null
+    };
 
-    expect(title).toBeInTheDocument;
+    it("matches snapshot", () => {
+      const generated = renderer
+        .create(
+          <Router>
+            <LoginForm
+              allUsers={validProps.allUsers}
+              setCurrentUser={validProps.setCurrentUser}
+              currentUser={validProps.currentUser}
+            />
+          </Router>
+        ).toJSON();
+
+      expect(generated).toMatchSnapshot();
+    })
+
+    it("renders title", () => {
+      render(
+        <Router>
+          <LoginForm
+            allUsers={validProps.allUsers}
+            setCurrentUser={validProps.setCurrentUser}
+            currentUser={validProps.currentUser}
+          />
+        </Router>
+      );
+      const title = screen.getByText(/Login here:/);
+
+      expect(title).toBeInTheDocument;
+    })
+
+    it("renders all form elements", () => {
+      render(
+        <Router>
+          <LoginForm
+            allUsers={validProps.allUsers}
+            setCurrentUser={validProps.setCurrentUser}
+            currentUser={validProps.currentUser}
+          />
+        </Router>
+      )
+      const emailField = screen.getByText("Email");
+      const passwordField = screen.getByText("Password");
+      const loginButton = screen.getByText("Submit");
+
+      expect(emailField).toBeInTheDocument;
+      expect(passwordField).toBeInTheDocument;
+      expect(loginButton).toBeInTheDocument;
+    })
+
+    it("shows login failure message with non existent user", async () => {
+      render(
+        <Router>
+          <LoginForm
+            allUsers={validProps.allUsers}
+            setCurrentUser={validProps.setCurrentUser}
+            currentUser={validProps.currentUser}
+          />
+        </Router>
+      )
+      const emailField = screen.getByLabelText("Email");
+      const passwordField = screen.getByLabelText("Password");
+      const loginButton = screen.getByText("Submit");
+
+      fireEvent.change(emailField, { target: { value: "test@email.com" } });
+      fireEvent.change(passwordField, { target: { value: "testPassword" } });
+      fireEvent.click(loginButton);
+
+      const errorMessage = await screen.findByText("Login failed");
+      expect(errorMessage).toBeInTheDocument;
+    })
+
+    it("shows login failure message when logging in with invalid credentials", async () => {
+      const updatedProps = {
+        allUsers: [
+          {
+            firstName: "testFirst",
+            lastName: "testLast",
+            email: "test@email.com",
+            password: "qwerty"
+          }
+        ],
+        setCurrentUser: jest.fn()
+      }
+      render(
+        <Router>
+          <LoginForm
+            allUsers={updatedProps.allUsers}
+            setCurrentUser={updatedProps.setCurrentUser}
+            currentUser={validProps.currentUser}
+          />
+        </Router>
+      )
+      const emailField = screen.getByLabelText("Email");
+      const passwordField = screen.getByLabelText("Password");
+      const loginButton = screen.getByText("Submit");
+
+      fireEvent.change(emailField, { target: { value: "test@email.com" } });
+      fireEvent.change(passwordField, { target: { value: "badPassword" } });
+      fireEvent.click(loginButton);
+
+      const errorMessage = await screen.findByText("Login failed");
+
+      expect(errorMessage).toBeInTheDocument;
+    })
   })
 
-  it("renders all form elements", () => {
-    render(
-      <Router>
-        <LoginForm
-          allUsers={validProps.allUsers}
-          setCurrentUser={validProps.setCurrentUser}
-        />
-      </Router>
-    )
-    const emailField = screen.getByText("Email");
-    const passwordField = screen.getByText("Password");
-    const loginButton = screen.getByText("Submit");
-
-    expect(emailField).toBeInTheDocument;
-    expect(passwordField).toBeInTheDocument;
-    expect(loginButton).toBeInTheDocument;
-  })
-
-  it("shows login failure message with non existent user", async () => {
-    render(
-      <Router>
-        <LoginForm
-          allUsers={validProps.allUsers}
-          setCurrentUser={validProps.setCurrentUser}
-        />
-      </Router>
-    )
-    const emailField = screen.getByLabelText("Email");
-    const passwordField = screen.getByLabelText("Password");
-    const loginButton = screen.getByText("Submit");
-
-    fireEvent.change(emailField, { target: { value: "test@email.com" } });
-    fireEvent.change(passwordField, { target: { value: "testPassword" } });
-    fireEvent.click(loginButton);
-
-    const errorMessage = await screen.findByText("Login failed");
-    expect(errorMessage).toBeInTheDocument;
-  })
-
-  it("shows login failure message when logging in with invalid credentials", async () => {
-    const updatedProps = {
-      allUsers: [
-        {
-          firstName: "testFirst",
-          lastName: "testLast",
-          email: "test@email.com",
-          password: "qwerty"
-        }
-      ],
-      setCurrentUser: jest.fn()
-    }
-    render(
-      <Router>
-        <LoginForm
-          allUsers={updatedProps.allUsers}
-          setCurrentUser={updatedProps.setCurrentUser}
-        />
-      </Router>
-    )
-    const emailField = screen.getByLabelText("Email");
-    const passwordField = screen.getByLabelText("Password");
-    const loginButton = screen.getByText("Submit");
-
-    fireEvent.change(emailField, { target: { value: "test@email.com" } });
-    fireEvent.change(passwordField, { target: { value: "badPassword" } });
-    fireEvent.click(loginButton);
-
-    const errorMessage = await screen.findByText("Login failed");
-
-    expect(errorMessage).toBeInTheDocument;
-  })
 });
--- src/__tests__/SignupForm.test.js
+++ src/__tests__/SignupForm.test.js
@@ -4,126 +4,171 @@ import renderer from "react-test-renderer";
 import SignupForm from '../components/SignupForm';
 
 describe("Signup form component", () => {
-  const validProps = {
-    allUsers: [],
-    setAllUsers: jest.fn(),
-    setCurrentUser: jest.fn()
-  };
-  it("matches snapshot", () => {
-    const generated = renderer
-      .create(
+  describe("User is logged in", () => {
+    const validProps = {
+      allUsers: [
+        {
+          firstName: "testFirst",
+          lastName: "testSecond",
+          email: "test@email.com",
+          password: "asdfgh"
+        }
+      ],
+      setCurrentUser: jest.fn(),
+      currentUser: {
+        firstName: "testFirst",
+        lastName: "testSecond",
+        email: "test@email.com",
+        password: "asdfgh"
+      }
+    };
+
+    it("renders already logged in message", () => {
+      render(
+        <Router>
+          <SignupForm
+              allUsers={validProps.allUsers}
+              setAllUsers={validProps.setAllUsers}
+              setCurrentUser={validProps.setCurrentUser}
+              currentUser={validProps.currentUser}
+            />
+        </Router>
+      );
+
+      const title = screen.getByText("You are already logged in, testFirst!");
+
+      expect(title).toBeInTheDocument;
+    })
+  })
+
+  describe("User is not logged in", () => {
+    const validProps = {
+      allUsers: [],
+      setAllUsers: jest.fn(),
+      setCurrentUser: jest.fn(),
+      currentUser: null
+    };
+    it("matches snapshot", () => {
+      const generated = renderer
+        .create(
+          <Router>
+            <SignupForm
+              allUsers={validProps.allUsers}
+              setAllUsers={validProps.setAllUsers}
+              setCurrentUser={validProps.setCurrentUser}
+              currentUser={validProps.currentUser}
+            />
+          </Router>
+        ).toJSON();
+
+      expect(generated).toMatchSnapshot();
+    })
+
+    it("renders title", () => {
+      render(
         <Router>
           <SignupForm
             allUsers={validProps.allUsers}
             setAllUsers={validProps.setAllUsers}
             setCurrentUser={validProps.setCurrentUser}
+            currentUser={validProps.currentUser}
           />
         </Router>
-      ).toJSON();
+      )
+      const title = screen.getByText(/Signup here:/);
 
-    expect(generated).toMatchSnapshot();
-  })
+      expect(title).toBeInTheDocument;
+    })
 
-  it("renders title", () => {
-    render(
-      <Router>
-        <SignupForm
-          allUsers={validProps.allUsers}
-          setAllUsers={validProps.setAllUsers}
-          setCurrentUser={validProps.setCurrentUser}
-        />
-      </Router>
-    )
-    const title = screen.getByText(/Signup here:/);
+    it("renders all form elements", () => {
+      render(
+        <Router>
+          <SignupForm
+            allUsers={validProps.allUsers}
+            setAllUsers={validProps.setAllUsers}
+            setCurrentUser={validProps.setCurrentUser}
+            currentUser={validProps.currentUser}
+          />
+        </Router>
+      )
+      const firstNameField = screen.getByText("First Name");
+      const lastNameField = screen.getByText("Last Name");
+      const emailField = screen.getByText("Email");
+      const passwordField = screen.getByText("Password");
+      const signupButton = screen.getByText("Submit");
 
-    expect(title).toBeInTheDocument;
-  })
+      expect(firstNameField).toBeInTheDocument;
+      expect(lastNameField).toBeInTheDocument;
+      expect(emailField).toBeInTheDocument;
+      expect(passwordField).toBeInTheDocument;
+      expect(signupButton).toBeInTheDocument;
+    })
 
-  it("renders all form elements", () => {
-    render(
-      <Router>
-        <SignupForm
-          allUsers={validProps.allUsers}
-          setAllUsers={validProps.setAllUsers}
-          setCurrentUser={validProps.setCurrentUser}
-        />
-      </Router>
-    )
-    const firstNameField = screen.getByText("First Name");
-    const lastNameField = screen.getByText("Last Name");
-    const emailField = screen.getByText("Email");
-    const passwordField = screen.getByText("Password");
-    const signupButton = screen.getByText("Submit");
+    it("shows signup failure message when registering existing user", async () => {
+      const updatedProps = {
+        allUsers: [
+          {
+            firstName: "testFirst",
+            lastName: "testLast",
+            email: "test@email.com",
+            password: "qwerty"
+          }
+        ],
+        setAllUsers: jest.fn(),
+        setCurrentUser: jest.fn()
+      };
 
-    expect(firstNameField).toBeInTheDocument;
-    expect(lastNameField).toBeInTheDocument;
-    expect(emailField).toBeInTheDocument;
-    expect(passwordField).toBeInTheDocument;
-    expect(signupButton).toBeInTheDocument;
-  })
+      render(
+        <Router>
+          <SignupForm
+            allUsers={updatedProps.allUsers}
+            setAllUsers={updatedProps.setAllUsers}
+            setCurrentUser={updatedProps.setCurrentUser}
+            currentUser={validProps.currentUser}
+          />
+        </Router>
+      )
+      const firstNameField = screen.getByLabelText("First Name");
+      const lastNameField = screen.getByLabelText("Last Name");
+      const emailField = screen.getByLabelText("Email");
+      const passwordField = screen.getByLabelText("Password");
+      const signupButton = screen.getByText("Submit");
 
-  it("shows signup failure message when registering existing user", async () => {
-    const updatedProps = {
-      allUsers: [
-        {
-          firstName: "testFirst",
-          lastName: "testLast",
-          email: "test@email.com",
-          password: "qwerty"
-        }
-      ],
-      setAllUsers: jest.fn(),
-      setCurrentUser: jest.fn()
-    };
+      fireEvent.change(firstNameField, { target: { value: updatedProps.allUsers[0].firstName } });
+      fireEvent.change(lastNameField, { target: { value: updatedProps.allUsers[0].lastName } });
+      fireEvent.change(emailField, { target: { value: updatedProps.allUsers[0].email } });
+      fireEvent.change(passwordField, { target: { value: updatedProps.allUsers[0].password } });
+      fireEvent.click(signupButton);
 
-    render(
-      <Router>
-        <SignupForm
-          allUsers={updatedProps.allUsers}
-          setAllUsers={updatedProps.setAllUsers}
-          setCurrentUser={updatedProps.setCurrentUser}
-        />
-      </Router>
-    )
-    const firstNameField = screen.getByLabelText("First Name");
-    const lastNameField = screen.getByLabelText("Last Name");
-    const emailField = screen.getByLabelText("Email");
-    const passwordField = screen.getByLabelText("Password");
-    const signupButton = screen.getByText("Submit");
+      const errorMessage = await screen.findByText("User already exists");
 
-    fireEvent.change(firstNameField, {target: {value: updatedProps.allUsers[0].firstName}});
-    fireEvent.change(lastNameField, {target: {value: updatedProps.allUsers[0].lastName}});
-    fireEvent.change(emailField, {target: {value: updatedProps.allUsers[0].email}});
-    fireEvent.change(passwordField, {target: {value: updatedProps.allUsers[0].password}});
-    fireEvent.click(signupButton);
+      expect(errorMessage).toBeInTheDocument;
+    })
 
-    const errorMessage = await screen.findByText("User already exists");
+    it("shows login failure message when all fields not filled out on submit", async () => {
+      render(
+        <Router>
+          <SignupForm
+            allUsers={validProps.allUsers}
+            setAllUsers={validProps.setAllUsers}
+            setCurrentUser={validProps.setCurrentUser}
+            currentUser={validProps.currentUser}
+          />
+        </Router>
+      )
+      const firstNameField = screen.getByLabelText("First Name");
+      const lastNameField = screen.getByLabelText("Last Name");
+      const passwordField = screen.getByLabelText("Password");
+      const signupButton = screen.getByText("Submit");
 
-    expect(errorMessage).toBeInTheDocument;
-  })
+      fireEvent.change(firstNameField, "testFirst");
+      fireEvent.change(lastNameField, "testLast");
+      fireEvent.change(passwordField, "testPassword");
+      fireEvent.click(signupButton);
 
-  it("shows login failure message when all fields not filled out on submit", async () => {
-    render(
-      <Router>
-        <SignupForm
-          allUsers={validProps.allUsers}
-          setAllUsers={validProps.setAllUsers}
-          setCurrentUser={validProps.setCurrentUser}
-        />
-      </Router>
-    )
-    const firstNameField = screen.getByLabelText("First Name");
-    const lastNameField = screen.getByLabelText("Last Name");
-    const passwordField = screen.getByLabelText("Password");
-    const signupButton = screen.getByText("Submit");
+      const errorMessage = await screen.findByText("All fields not filled out");
 
-    fireEvent.change(firstNameField, "testFirst");
-    fireEvent.change(lastNameField, "testLast");
-    fireEvent.change(passwordField, "testPassword");
-    fireEvent.click(signupButton);
-
-    const errorMessage = await screen.findByText("All fields not filled out");
-
-    expect(errorMessage).toBeInTheDocument;
+      expect(errorMessage).toBeInTheDocument;
+    })
   })
 });
